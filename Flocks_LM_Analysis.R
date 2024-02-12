eda04922ce302783fad06f889d51fd8850aadab8
A.M. Pitera <78067457+apitera@users.noreply.github.com>
2024-02-12 12:29:46 -0500

    Annotated linear models and removed old analyses

--- Flocks_LM_Analysis.R
+++ Flocks_LM_Analysis.R
@@ -8,7 +8,6 @@
 # Libraries used: -----
 library(car)
 library(emmeans)
-library(ggplot2)
 library(lme4)
 library(lmerTest)
 
@@ -23,93 +22,54 @@ load(paste0(dat.path, dat.file))
 
 # Let's run a model to see if flock treatment -----
 
-
+# model with 3-way interaction + NestedArray
 int.mod   <- lm(FL.20.LocErr ~ AssignmentType*Elevation*I.20.LocErr+Elevation/NestedArrayAssigned, data = TestingResults)
+# 2-way interactions + NestedArray
 t.int.mod <-lm(FL.20.LocErr ~ (AssignmentType+Elevation+I.20.LocErr)^2 +Elevation/NestedArrayAssigned, data = TestingResults)
 
-anova(int.mod, t.int.mod)
+anova(int.mod, t.int.mod)# no significant differences between models, dropping 3-way interaction
 
-
+# Interactions only between two sets of fixed effects + NestedArray
 no.ai.mod <- lm(FL.20.LocErr ~ AssignmentType*Elevation+Elevation*I.20.LocErr + Elevation/NestedArrayAssigned, data = TestingResults)
-anova(t.int.mod, no.ai.mod)
+anova(t.int.mod, no.ai.mod) # no significant differences between models, dropping initial testing performance * Assignment type (treatment) interaction
 
+# Removing all interactions but keeping array nested within elevation
 no.int <- lm(FL.20.LocErr ~ AssignmentType+Elevation+I.20.LocErr + Elevation/NestedArrayAssigned, data = TestingResults)
-anova(no.ai.mod, no.int)
+anova(no.ai.mod, no.int)# no significant differences between models, dropping all interactions
 
+# removing array
 no.arr <- lm(FL.20.LocErr ~ AssignmentType+Elevation+I.20.LocErr, data = TestingResults)
-anova(no.arr, no.int)
+anova(no.arr, no.int)# no significant differences between models, dropping array
 
 Anova(no.arr, type = 3)
 summary(no.arr)$adj.r.squared
 
-anova(no.int, int.mod) # no diffs b/n these two, cut interaction???
-
-
-no.arr <- lm(FL.20.LocErr ~ AssignmentType+Elevation, data = TestingResults)
-car::Anova(no.arr, type = 3)
-anova(no.int, no.arr) # no diffs, drop array
 
 
 
-
-
-# Relationships between initial & reversal testing and flock testing...
-# *** JFW says to keep AssignmentType in there 
-
-Anova(lm(FL.20.LocErr ~  I.20.LocErr * Elevation * AssignmentType    + Elevation/NestedArrayAssigned, data = TestingResults), type = 3)
-Anova(lm(FL.20.LocErr ~ (I.20.LocErr + Elevation + AssignmentType)^2 + Elevation/NestedArrayAssigned, data = TestingResults), type = 3)
-
-
-
-car::Anova(init, type = 3)
-init        <- lm(FL.20.LocErr ~ I.20.LocErr * Elevation + AssignmentType + Elevation/NestedArrayAssigned, data = TestingResults)
-no.int.init <- lm(FL.20.LocErr ~ I.20.LocErr + Elevation + AssignmentType + Elevation/NestedArrayAssigned, data = TestingResults)
-
-car::Anova(no.int.init, type = 3)
-anova(init, no.int.init) # no diffs, so dropping Array
-
-nar.init <- lm(FL.20.LocErr ~ I.20.LocErr + Elevation + AssignmentType, data = TestingResults)
-anova(nar.init, no.int.init) # no diffs, so dropping Assignment type
-
-car::Anova(nar.init, type = 3)
-summary(nar.init)$adj.r.squared
-
-nass.init <- lm(FL.20.LocErr ~ I.20.LocErr + Elevation, data = TestingResults)
-summary(nass.init)$adj.r.squared
-car::Anova(nass.init, type = 3)
-anova(nass.init, nar.init) # no diffs b/n model with Elevation and elevation/array and without. Going without
-
-ne.init <- lm(FL.20.LocErr ~ I.20.LocErr, data = TestingResults)
-anova(ne.init, nass.init) # no.diffs, moving on with removing elevation.
-
-car::Anova(ne.init, type = 3)
-summary(ne.init)$adj.r.squared
 
 
 # Looking at a repeated measures learning curve...
 
-
-
-# including 3 trials ----
 a.curve <- lmer(MeanLocErr ~ nTrials * AssignmentType * Elevation + (1|BirdID), data = longTestingResults)
-#summary(a.curve)
+
 Anova(a.curve, type = "III", test.statistic = "F")
 
 b.curve <- lmer(MeanLocErr ~ (nTrials + AssignmentType + Elevation)^2 + (1|BirdID), data = longTestingResults)
-#summary(b.curve)
+
 car::Anova(b.curve, type = 3, test.statistic = "F")
-anova(a.curve, b.curve) # no diffs, drop
+anova(a.curve, b.curve) # no diffs, dropping 3-way interaction
 
 c.curve <- lmer(MeanLocErr ~ nTrials * Elevation + nTrials * AssignmentType + (1|BirdID), data = longTestingResults)
-#summary(c.curve)
-car::Anova(c.curve, type = "III", test.statistic = "F")
-anova(b.curve, c.curve) # no diffs, drop...
 
-d.curve <- lmer(MeanLocErr ~ nTrial s+ Elevation + AssignmentType + (1|BirdID), data = longTestingResults)
+car::Anova(c.curve, type = "III", test.statistic = "F")
+anova(b.curve, c.curve) # no diffs, drop interactions
+
+d.curve <- lmer(MeanLocErr ~ nTrials+ Elevation + AssignmentType + (1|BirdID), data = longTestingResults)
 #summary(d.curve)
 car::Anova(d.curve, type = "III", test.statistic = "F")
 
-anova(c.curve, d.curve)
+anova(c.curve, d.curve) # no difference but reporting results from both c & d models
 
 
 nar.emm <- emmeans(c.curve, ~ nTrials * Elevation) 
